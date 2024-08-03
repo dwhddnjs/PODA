401a7933b094c7d63d8de5c7e2641ca51f240248
dwhddnjs <per1215@naver.com>
2024-08-03 18:53:12 +0900

    ✨ Feat : ReactQuery 추가 및 예제코드 첨부 #2

---
+++ hooks/mutation/post.ts
@@ -0,0 +1,12 @@
+import { postRequest } from "@/lib/protocol"
+import { useMutation } from "@tanstack/react-query"
+
+export const useAddPost = () => {
+  const { mutate, isPending, isError } = useMutation({
+    mutationFn: async (data: any) => await postRequest("/posts", data),
+    // onSuccess: () => navigate("/info"),
+    onError: (error) => console.log(error),
+  })
+
+  return { mutate, isPending, isError }
+}
