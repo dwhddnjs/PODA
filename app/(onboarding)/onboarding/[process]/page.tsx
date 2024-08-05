49476d4791032b283e19185d0771f5dde5f00ec1
qpsqps123 <55061153+qpsqps123@users.noreply.github.com>
2024-08-05 19:37:13 +0900

    ♻️ Modify : Title 공용 컴포넌트 대신 h1 태그 전역 스타일링으로 변경 #14

--- app/(onboarding)/onboarding/[process]/page.tsx
+++ app/(onboarding)/onboarding/[process]/page.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import Desc from "@/components/desc"
-import Title from "@/components/title"
 import { Button } from "@/components/ui/button"
 import Image from "next/image"
 import { useRouter } from "next/navigation"
@@ -26,37 +25,39 @@ export default function OnboardingPage({
       <div className="p-6">
         {Number(params.process) === 1 && (
           <>
-            {" "}
-            <Title
-              content={`오늘 당신의\n하루는 어떠셨나요`}
-              restStyles={`mt-2`}
-            />
+            <h1 className={`mt-2`}>
+              오늘 당신의
+              <br />
+              하루는 어떠셨나요
+            </h1>
             <Desc
               content={`상황 스티커를 선택하고 글과 사진을 남겨보세요\n빠르고 간편하게 당신의 하루를 기록해 남길 수 있어요`}
-              restStyles={`mt-2`}
+              className={`mt-2`}
             />
           </>
         )}
         {Number(params.process) === 2 && (
           <>
-            {" "}
-            <Title
-              content={`지나간 감정들을\n한 눈에 살펴보세요`}
-              restStyles={`mt-2`}
-            />
+            <h1 className={`mt-2`}>
+              지나간 감점들을
+              <br />한 눈에 살펴보세요
+            </h1>
             <Desc
               content={`한 달 동안 당신의 기분이 어땠는 지 차트로 확인해보세요\n일기를 자주 기록하면 통계가 초록색으로 채워져요`}
-              restStyles={`mt-2`}
+              className={`mt-2`}
             />
           </>
         )}
         {Number(params.process) === 3 && (
           <>
-            {" "}
-            <Title content={`내가 쓴 일기를\n보여주세요`} restStyles={`mt-2`} />
+            <h1 className={`mt-2`}>
+              내가 쓴 일기를
+              <br />
+              보여주세요
+            </h1>
             <Desc
               content={`다양한 사람들과 자신이 기록한 일기를 교환해보세요\n선택한 조건에 맞춰 일기를 공유할 수 있어요 `}
-              restStyles={`mt-2`}
+              className={`mt-2`}
             />
           </>
         )}
--- app/(onboarding)/start/page.tsx
+++ app/(onboarding)/start/page.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import Desc from "@/components/desc"
-import Title from "@/components/title"
 import { Button } from "@/components/ui/button"
 import Image from "next/image"
 import { useRouter } from "next/navigation"
@@ -18,13 +17,14 @@ export default function StartPage() {
         className="w-full"
       />
       <div className="p-6">
-        <Title
-          content={`일기를 기록하고\n마음을 전하세요`}
-          restStyles={`mt-2`}
-        />
+        <h1 className={`mt-2`}>
+          일기를 기록하고
+          <br />
+          마음을 전하세요
+        </h1>
         <Desc
           content={`바쁜 현대 사회에 일기 쓰기 귀찮은 당신\n간편하게 일기를 기록하고 다양한 사람들과 공유해보세요`}
-          restStyles={`mt-2`}
+          className={`mt-2`}
         />
       </div>
       <Button
