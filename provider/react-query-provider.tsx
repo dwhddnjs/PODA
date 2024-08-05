0fd0f0c0b29bab07ab8a107f0fa16db5e1b93876
dwhddnjs <per1215@naver.com>
2024-08-05 09:46:19 +0900

    :gear: Cofing : PWA 재설정 (#2)

--- provider/react-query-provider.tsx
+++ provider/react-query-provider.tsx
@@ -2,6 +2,7 @@
 
 import React from "react"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 
 const queryClient = new QueryClient()
 
@@ -12,6 +13,9 @@ export default function ReactQueryProvider({
   children: React.ReactNode
 }) {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
