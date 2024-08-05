22957f89882986913ad35d02e7df96ecfa1eb042
dwhddnjs <per1215@naver.com>
2024-08-05 12:19:20 +0900

    :gear: Config : 세그먼트 별 폴더 추가 (#4)

---
+++ app/(main)/exchange-diary/page.tsx
@@ -0,0 +1,8 @@
+"use client"
+
+import { BottomNavigation } from "@/components/bottom-navigation"
+import React from "react"
+
+export default function ExchangeDiaryPage() {
+  return <div>교환일기</div>
+}
