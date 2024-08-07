d433dd53a8d68f8520a5e18e893ef01fb17bfd49
qpsqps123 <55061153+qpsqps123@users.noreply.github.com>
2024-08-07 17:02:59 +0900

    💄 Style : get-extra-user-info 페이지에서 사용하는 진행 상태 표시 컴포넌트 추가 #42

---
+++ app/(onboarding)/get-extra-user-info/process-status-green.tsx
@@ -0,0 +1,23 @@
+import { usePathname } from "next/navigation"
+
+export const ProcessStatusGreen = () => {
+  const pathname = usePathname()
+
+  const lightOn = <div className="w-14 h-2 bg-mainColor"></div>
+  const lightOff = <div className="w-14 h-2 bg-gray-300"></div>
+
+  return (
+    <div className="flex justify-center gap-3 py-14 mx-auto">
+      {lightOn}
+      {pathname.includes("radio/2") ||
+      pathname.includes("select/1") ||
+      pathname.includes("multi-select/1")
+        ? lightOn
+        : lightOff}
+      {pathname.includes("select/1") || pathname.includes("multi-select/1")
+        ? lightOn
+        : lightOff}
+      {pathname.includes("multi-select/1") ? lightOn : lightOff}
+    </div>
+  )
+}
