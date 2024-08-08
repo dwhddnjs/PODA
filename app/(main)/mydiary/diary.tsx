e9e7088e2fecc0f290e61a16896334bb81cd882b
seok-hyung <korea970223@naver.com>
2024-08-08 10:02:21 +0900

    :lipstick:Style : diary component css 수정 (#44)

--- app/(main)/mydiary/diary.tsx
+++ app/(main)/mydiary/diary.tsx
@@ -37,98 +37,96 @@ type DiaryProps = {
 
 export const Diary = ({ diaryData }: DiaryProps) => {
   return (
-    <div className="py-6 bg-backgroundLighter rounded-xl">
-      {diaryData.map((item, index) => (
-        <>
-          <div key={item.id}>
-            <div className="flex gap-2 px-4 relative">
-              {/* 드롭다운 메뉴 */}
-              <div className="absolute right-0 top-0 text-primary">
-                <Dropdown />
-              </div>
-              {/* 왼쪽 */}
-              <div className="flex items-center flex-col flex-shrink-0">
+    <div className="bg-backgroundLighter rounded-xl">
+      {diaryData.map((item, index) => {
+        console.log(getTxtcolorClasses(item.mood))
+        return (
+          <>
+            <div
+              key={item.id}
+              className="pt-[16px] first:pt-[24px] last:pb-[24px]">
+              <div className="flex gap-3 px-4 relative">
+                {/* 드롭다운 메뉴 */}
+                <div className="absolute right-0 top-0 text-primary">
+                  <Dropdown />
+                </div>
+                {/* 왼쪽 */}
+                <div className="flex items-center flex-col flex-shrink-0">
+                  <div>
+                    <Image
+                      width={48}
+                      height={48}
+                      src={getImgByMood(item.mood)}
+                      alt="감정 이미지"
+                    />
+                  </div>
+                  {/* 이모션 세로 선 */}
+                  <div className="flex flex-col h-full pt-[16px]">
+                    {index < diaryData.length - 1 && (
+                      <div className="h-full border-[1.2px] border-[#666666]"></div>
+                    )}
+                  </div>
+                </div>
+                {/* 오른쪽 */}
                 <div>
-                  <Image
-                    width={48}
-                    height={48}
-                    src={getImgByMood(item.mood)}
-                    alt="감정 이미지"
-                  />
-                </div>
-                {/* 이모션 세로 선 */}
-                <div className="flex flex-col my-2 h-full">
-                  {index < diaryData.length - 1 && (
-                    <div className="h-full w-1 rounded-full bg-[#555555]"></div>
-                  )}
-                </div>
-              </div>
-              {/* 오른쪽 */}
-              <div>
-                {/* 이미지텍스트, 시간 */}
-                <div className="flex gap-2 items-center mb-2">
-                  <h2
-                    className={cn(
-                      `text-2xl`,
-                      `text-emotion-${item.mood}`,
-                      item.mood === "happy" && "text-emotion-happy",
-                      item.mood === "calm" && "text-emotion-calm",
-                      item.mood === "sad" && "text-emotion-sad",
-                      item.mood === "annoy" && "text-emotion-annoy",
-                      item.mood === "angry" && "text-emotion-angry"
-                    )}>
-                    {getTxtByMood(item.mood)}
-                  </h2>
-                  <div className="text-secondary text-sm">{item.time}</div>
-                </div>
-                {/* 상황 태그들 */}
-                <ul className="flex gap-2 flex-wrap mb-2">
-                  {item.tags.map((tagItem, tagIndex) => {
-                    return (
-                      <li
-                        key={tagIndex}
-                        className="px-2 flex border-solid border-[1px] border-secondary rounded-[20px]">
-                        <Image
-                          src={tagItem.tagImg}
-                          className="mr-1"
-                          alt="태그 아이콘"
-                          width={16}
-                          height={16}
-                        />
-                        <small className="text-secondary">
-                          {tagItem.tagTxt}
-                        </small>
-                      </li>
-                    )
-                  })}
-                </ul>
+                  {/* 이미지텍스트, 시간 */}
+                  <div className="flex gap-2 items-center mb-2">
+                    <h2 className={`text-2xl ${getTxtcolorClasses(item.mood)}`}>
+                      {getTxtByMood(item.mood)}
+                    </h2>
+                    <div className="text-secondary text-sm text-emotion-happy">
+                      {item.time}
+                    </div>
+                  </div>
+                  {/* 상황 태그들 */}
+                  <ul className="flex gap-2 flex-wrap mb-2">
+                    {item.tags.map((tagItem, tagIndex) => {
+                      return (
+                        <li
+                          key={tagIndex}
+                          className="px-2 flex border-solid border-[1px] border-secondary rounded-[20px]">
+                          <Image
+                            src={tagItem.tagImg}
+                            className="mr-1"
+                            alt="태그 아이콘"
+                            width={16}
+                            height={16}
+                          />
+                          <small className="text-secondary">
+                            {tagItem.tagTxt}
+                          </small>
+                        </li>
+                      )
+                    })}
+                  </ul>
 
-                {/* 디스크립션 */}
-                {item.description.title && <h3>{item.description.title}</h3>}
-                <p className="text-primary text-xs">
-                  {item.description.content}
-                </p>
-                {/* 업로드한 이미지들 */}
-                <div className="flex flex-wrap w-full">
-                  {item.uploadImgs.map((imgItem) => {
-                    return (
-                      <div key={index} className="w-1/2 p-1">
-                        <Image
-                          src={imgItem}
-                          width={160}
-                          height={160}
-                          alt="업로드 이미지"
-                          className="w-full h-auto object-cover"
-                        />
-                      </div>
-                    )
-                  })}
+                  {/* 디스크립션 */}
+                  {item.description.title && <h3>{item.description.title}</h3>}
+                  <p className="text-primary text-xs">
+                    {item.description.content}
+                  </p>
+                  {/* 업로드한 이미지들 */}
+                  <div className="flex flex-wrap w-full">
+                    {item.uploadImgs.map((imgItem) => {
+                      return (
+                        <div key={index} className="w-1/2 p-1">
+                          <Image
+                            src={imgItem}
+                            width={160}
+                            height={160}
+                            alt="업로드 이미지"
+                            className="w-full h-auto object-cover"
+                          />
+                        </div>
+                      )
+                    })}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </>
-      ))}
+          </>
+        )
+      })}
     </div>
   )
 }
--- lib/function/get-txtcolor-classes.ts
+++ lib/function/get-txtcolor-classes.ts
@@ -2,13 +2,28 @@ import { MoodType } from "@/app/(main)/mydiary/diary"
 import { cn } from "@/lib/utils"
 
 export const getTxtcolorClasses = (mood: keyof MoodType) => {
-  return cn(
-    `text-2xl`,
-    `text-emotion-${mood}`,
-    mood === "happy" && "text-emotion-happy",
-    mood === "calm" && "text-emotion-calm",
-    mood === "sad" && "text-emotion-sad",
-    mood === "annoy" && "text-emotion-annoy",
-    mood === "angry" && "text-emotion-angry"
-  )
+  return `text-emotion-${mood}`
 }
+
+// let classes = ""
+// if (mood === "happy") {
+//   classes = "text-emotion-happy"
+// } else if (mood === "calm") {
+//   classes = "text-emotion-calm"
+// } else if (mood === "sad") {
+//   classes = "text-emotion-sad"
+// } else if (mood === "annoy") {
+//   classes = "text-emotion-annoy"
+// } else if (mood === "angry") {
+//   classes = "text-emotion-angry"
+// }
+// return classes
+
+// return cn(
+//   `text-emotion-${mood}`,
+//   mood === "happy" && "text-emotion-happy",
+//   mood === "calm" && "text-emotion-calm",
+//   mood === "sad" && "text-emotion-sad",
+//   mood === "annoy" && "text-emotion-annoy",
+//   mood === "angry" && "text-emotion-angry"
+// )
