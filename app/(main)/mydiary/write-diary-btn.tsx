44f37af43b61ecf64a121b0600f7517b2b22ce06
seok-hyung <korea970223@naver.com>
2024-08-09 15:46:44 +0900

    :sparkles:Feat : 작성하기 버튼 추가

---
+++ app/(main)/mydiary/write-diary-btn.tsx
@@ -0,0 +1,19 @@
+"use client"
+import React from "react"
+import { Pencil } from "lucide-react"
+import { useRouter } from "next/navigation"
+
+export const WriteDiaryBtn = () => {
+  const router = useRouter()
+  return (
+    <div className="fixed bottom-4 right-4 z-10 bg-mainColor p-3 rounded-full">
+      <div
+        className=" boxshadow"
+        onClick={() => {
+          router.push("/write-diary")
+        }}>
+        <Pencil size={28} />
+      </div>
+    </div>
+  )
+}
