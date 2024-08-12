5b09e2ea8aad1f14f37ad0ac059c0e10d815a104
dwhddnjs <per1215@naver.com>
2024-08-12 21:23:03 +0900

    ✨ Feat : User 쿼리 추가

---
+++ hooks/query/user.ts
@@ -0,0 +1,20 @@
+import { apiKeys } from "@/lib/api-keys"
+import { fetcher } from "@/lib/protocol"
+import { useQuery } from "@tanstack/react-query"
+
+export const useUser = (id: string) => {
+  const { data, isPending, error, refetch } = useQuery({
+    queryKey: [apiKeys.user, id],
+    queryFn: async () => {
+      return await fetcher(`/users/${id}`)
+    },
+    staleTime: 1000 * 3,
+  })
+
+  return {
+    data,
+    isPending,
+    error,
+    refetch,
+  }
+}
