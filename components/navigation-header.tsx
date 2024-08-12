605ebb8c5dee885212c38dbe45717bd6f8bb88cf
seok-hyung <korea970223@naver.com>
2024-08-12 11:05:59 +0900

    :lipstick:Style : isMood, isSave 추가 (#66)

--- components/navigation-header.tsx
+++ components/navigation-header.tsx
@@ -5,22 +5,42 @@ import { Button } from "./ui/button"
 import { ChevronLeft, Search } from "lucide-react"
 import { cn } from "@/lib/utils"
 import { useRouter } from "next/navigation"
+import Image from "next/image"
+import { useSelectedDiaryMood } from "@/hooks/store/use-seleceted-mood"
 
 interface NavigationHeaderProps {
+  isMood?: boolean
+  isSave?: boolean
   isDate?: boolean
   isSearch?: boolean
 }
 
+const handleBtnClick = () => {}
+
 export const NavigationHeader = ({
+  isMood,
+  isSave,
   isDate,
   isSearch,
 }: NavigationHeaderProps) => {
+  const { selectedMood } = useSelectedDiaryMood()
   const { back } = useRouter()
   return (
     <div className="w-full flex justify-between items-center py-[12px] ">
-      <Button variant="ghost" onClick={back}>
-        <ChevronLeft className="text-primary" width={28} height={28} />
-      </Button>
+      <div className="flex">
+        <Button variant="ghost" onClick={back}>
+          <ChevronLeft className="text-primary" width={28} height={28} />
+        </Button>
+        {isMood && (
+          <Image
+            width={28}
+            height={28}
+            src={`/assets/svg/${selectedMood}.svg`}
+            alt="선택한 감정상태"
+            className="-ml-1"
+          />
+        )}
+      </div>
 
       <h2 className={cn("text-primary", !isDate && "hidden ")}>2024년 7월</h2>
 
@@ -33,6 +53,21 @@ export const NavigationHeader = ({
           width={24}
           height={24}
         />
+      </Button>
+
+      <Button
+        variant="ghost"
+        disabled={!isSave}
+        className={cn("text-primary flex gap-2", !isSave && "hidden")}
+        onClick={handleBtnClick}>
+        <Image
+          width={28}
+          height={28}
+          src={"/assets/svg/checkBtn.svg"}
+          alt="저장버튼"
+          className={cn(!isSave && "hidden")}
+        />
+        저장
       </Button>
     </div>
   )
