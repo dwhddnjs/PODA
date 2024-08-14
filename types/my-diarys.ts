d7d40ea798d328f02416b3fa69f66a14c8816a22
dwhddnjs <per1215@naver.com>
2024-08-14 10:08:38 +0900

    ✨ Feat : 다이어리 타입 변경

--- types/my-diarys.ts
+++ types/my-diarys.ts
@@ -1,3 +1,5 @@
+import { User } from "./user"
+
 export type MoodType = {
   happy: string
   calm: string
@@ -21,6 +23,27 @@ export type Diary = {
   description?: Description
   uploadImgs?: string[]
   isHighlighted?: boolean
+}
+
+export type ImageTypes = {
+  path: string
+  name: string
+  originalname: string
+}
+
+export type DiaryTypes = {
+  _id: number
+  content: string
+  createdAt: string
+  updatedAt: string
+  mainImages: ImageTypes[]
+  title?: string
+  type: string
+  user: Pick<User, "_id" | "image" | "name">
+  extra: {
+    mood: keyof MoodType
+    tag: string[]
+  }
 }
 
 export interface MyDiary {
