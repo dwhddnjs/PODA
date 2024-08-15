93d51c0b83ed269aad89443175136b2b8f3e4ce5
dwhddnjs <per1215@naver.com>
2024-08-15 23:04:46 +0900

    ✨ Feat : 데이터 패칭 후 컴포넌트 수정

--- app/(main)/_components/exchange-diary.tsx
+++ app/(main)/_components/exchange-diary.tsx
@@ -8,6 +8,7 @@ import { DiaryTag } from "@/components/diary-tag"
 import { usePathname, useRouter } from "next/navigation"
 import { Diary } from "@/components/diary"
 import { DiaryTypes } from "@/types/my-diarys"
+import { getKoTime } from "@/lib/function/format-time"
 
 interface ExchangeDiaryProps {
   diaryDatas: DiaryTypes[]
@@ -21,8 +22,10 @@ export const ExchangeDiary = ({ diaryDatas }: ExchangeDiaryProps) => {
     <div
       onClick={() => pathname === "/exchange-diary/load-diary" && back()}
       className="bg-backgroundLighter rounded-lg pt-[20px] pb-[8px]">
-      {diaryDatas.map((diaryData, index) => (
-        <div className="flex gap-3 px-4 relative pb-[12px]" key={diaryData._id}>
+      {diaryDatas?.map((diaryData, index) => (
+        <div
+          className="flex gap-3 px-4 relative pb-[12px]"
+          key={diaryData?._id}>
           {/* 드롭다운 메뉴 */}
           <div className="absolute right-0 -top-1 text-primary">
             <Dropdown />
@@ -33,13 +36,13 @@ export const ExchangeDiary = ({ diaryDatas }: ExchangeDiaryProps) => {
               <Image
                 width={48}
                 height={48}
-                src={getImgByMood(diaryData.extra.mood)}
+                src={getImgByMood(diaryData?.extra?.mood)}
                 alt="감정 이미지"
               />
             </div>
             {/* 이모션 세로 선 */}
             <div className="flex flex-col h-full pt-[12px] ">
-              {diaryDatas.length > 1 && index < diaryDatas.length - 1 && (
+              {diaryDatas?.length > 1 && index < diaryDatas?.length - 1 && (
                 <div className="h-full border-[1.2px] border-[#666666]" />
               )}
             </div>
@@ -48,34 +51,27 @@ export const ExchangeDiary = ({ diaryDatas }: ExchangeDiaryProps) => {
           <div className="flex flex-col">
             {/* 이미지텍스트, 시간 */}
             <div className="flex gap-2 items-center mb-2">
-              <h2 className={`text-md text-emotion-${diaryData.extra.mood}`}>
-                {getTxtByMood(diaryData.extra.mood)}
+              <h2 className={`text-md text-emotion-${diaryData?.extra?.mood}`}>
+                {getTxtByMood(diaryData?.extra?.mood)}
               </h2>
               <div className="text-secondary text-sm">
-                {diaryData.createdAt}
+                {getKoTime(diaryData?.createdAt)}
               </div>
             </div>
 
             {/* 상황 태그들 */}
             <ul
-              className={`flex gap-1 flex-wrap mb-4 text-emotion-${diaryData.extra.mood}`}>
+              className={`flex gap-1 flex-wrap mb-4 text-emotion-${diaryData?.extra?.mood}`}>
               {diaryData?.extra?.tag &&
-                diaryData.extra?.tag.map((tagName, tagIndex) => {
-                  return (
-                    <DiaryTag
-                      key={tagName}
-                      tagName={tagName}
-                      mood={diaryData.extra.mood}
-                      tagIndex={tagIndex}
-                    />
-                  )
+                diaryData?.extra?.tag.map((tagName, tagIndex) => {
+                  return <DiaryTag key={tagName} tagName={tagName} />
                 })}
             </ul>
 
             {/* 디스크립션 */}
             <div className="mb-2">
-              {diaryData.title && <h3>{diaryData.title}</h3>}
-              <p className="text-primary text-xs">{diaryData.content}</p>
+              {diaryData?.title && <h3>{diaryData?.title}</h3>}
+              <p className="text-primary text-xs">{diaryData?.content}</p>
             </div>
             {/* 업로드한 이미지들 */}
             {/* <div className="flex flex-wrap w-full gap-3">
