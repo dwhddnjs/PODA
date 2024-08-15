e166d1af381ec11d26eade1cb38d8c5f8fb826c0
dwhddnjs <per1215@naver.com>
2024-08-15 23:09:32 +0900

    :pencil2: Modify : 각 개체 별 타입 수정

--- types/exchange-diary.ts
+++ types/exchange-diary.ts
@@ -1,23 +1,42 @@
-import { Diary } from "./my-diarys"
+// import { ExchangeDiary } from "@/app/(main)/_components/ExchangeDiary"
+// import { Diary } from "./my-diarys"
 import { User } from "./user"
 
-type DeliveryStatusItem = "completed" | "delivery"
+export type DeliveryStatusTypes = "completed" | "delivery"
 
 type DeliveryDetail = {
   id: number
   image: string
   user: Pick<User, "id" & "image" & "name">
   createdAt: string
-  Diary: Diary[]
+  //   Diary: Diary[]
 }
 
 export type ExchangeDiary = {
   id: number
-  status: DeliveryStatusItem
+  status: DeliveryStatusTypes
   user: Pick<User, "id" & "image" & "name">
   target: Pick<User, "id" & "image" & "name">
   interest: string[]
   createdAt: string
   updatedAt: string
   DeliveryDetails: DeliveryDetail[]
+}
+
+export type ExchangeDiaryTypes = {
+  _id: number
+  //   active: boolean
+  //   price: number
+  //   quantity: number
+  //   show: boolean
+  content: string
+  createdAt: string
+  updatedAt: string
+  extra: {
+    status: DeliveryStatusTypes
+    interest: string[]
+  }
+  seller: Pick<User, "_id" | "image" | "name">
+  userId: number
+  replies: number
 }
