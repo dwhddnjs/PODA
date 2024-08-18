98f174230e988fa0b002456ba62956890a82282e
dwhddnjs <per1215@naver.com>
2024-08-19 00:48:07 +0900

    🎨 Modify : 바텀 시트 필요 기능 수정

--- components/tag.tsx
+++ components/tag.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { useSelectedDiary } from "@/hooks/store/use-selected-diary"
 import { useUserData } from "@/hooks/store/use-user-data"
 import { cn } from "@/lib/utils"
 import React, { useState } from "react"
@@ -8,16 +9,19 @@ export const Tag = ({ children }: { children: React.ReactNode }) => {
   const [selected, setSelected] = useState<null | React.ReactNode>(null)
   console.log("selected: ", selected)
   const { addUserInterest, removeUserInterest } = useUserData()
+  const { removeInterest, setInterest } = useSelectedDiary()
 
   const handleSelectTag = (e: React.MouseEvent<HTMLLabelElement>) => {
     const target = e.target as HTMLLabelElement
     if (target.id) {
       setSelected(target.id)
       addUserInterest(target.id)
+      setInterest(target.id)
     }
     if (target.id === selected) {
       setSelected(null)
       removeUserInterest(target.id)
+      removeInterest(target.id)
     }
   }
 
@@ -26,7 +30,7 @@ export const Tag = ({ children }: { children: React.ReactNode }) => {
       onClick={(e) => handleSelectTag(e)}
       id={children as string}
       className={cn(
-        "flex justify-center items-center border-2 border-secondary rounded-2xl text-secondary font-black text-sm px-[12px] h-[30px] mb-2.5 ml-2 space-x-1",
+        "inline-flex justify-center items-center border-2 border-secondary rounded-2xl text-secondary font-black text-sm px-[12px] h-[30px] mb-2.5 ml-2 space-x-1",
         selected && "bg-emotion-happy text-black border-emotion-happy"
       )}>
       {children}
