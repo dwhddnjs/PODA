ac0a4881021f50a975f19e4b3c258e56d1e2efa9
seok-hyung <korea970223@naver.com>
2024-08-19 09:08:19 +0900

    :sparkles:Feat : 다이어리 수정을 위한 전역변수 추가 (#86)

--- hooks/store/use-diary.tsx
+++ hooks/store/use-diary.tsx
@@ -1,25 +1,69 @@
 import { create } from "zustand"
 
-// 관계, 활동, 날씨, 감정, 컨디션
-
 type DiaryValue = {
+  isEditMode?: boolean
+  _id?: number
+  createdAt?: string
+  updatedAt?: string
+  moodVal: string
+  user?: {
+    _id: number
+    name: string
+    image: string
+  }
   selectedTags?: string[]
   noteTitleVal?: string
   noteContentVal?: string
-  // cameraInput?: string // 촬영한 사진
-  // uploadImages?: string[] // 갤러리에서 불러온 사진들
+
   // setSelectedTags: (tags: TagDetails[]) => void
   // setNoteTitleVal: (value: string) => void
-  // setNoteContentVal: (value: string) => void
-  // setCameraInput: (value: string) => void
-  // setUploadImages: (images: string[]) => void
   seter: (value: any, type: keyof DiaryValue) => void
   resetValues: () => void
 }
 export const useDiaryValues = create<DiaryValue>((set) => ({
+  isEditMode: false,
+  _id: 0,
+  createdAt: "",
+  updatedAt: "",
+  user: {
+    _id: 0,
+    name: "",
+    image: "",
+  },
+  moodVal: "",
   selectedTags: [],
   noteTitleVal: "",
   noteContentVal: "",
+
+  seter: (value, type) => {
+    if (type === "user") {
+      set((state) => ({
+        user: {
+          ...state.user,
+          ...value, // value가 user 객체의 일부일 경우 병합
+        },
+      }))
+    } else {
+      set({ [type]: value })
+    }
+  },
+  resetValues: () => {
+    set({
+      _id: 0,
+      createdAt: "",
+      updatedAt: "",
+      user: {
+        _id: 0,
+        name: "",
+        image: "",
+      },
+      moodVal: "",
+      selectedTags: [],
+      noteTitleVal: "",
+      noteContentVal: "",
+    })
+  },
+
   // cameraInput: "",
   // uploadImages: [],
 
@@ -30,45 +74,36 @@ export const useDiaryValues = create<DiaryValue>((set) => ({
   // setSelectedTags: (tags) => set({ selectedTags: tags }),
   // setNoteTitleVal: (value) => set({ noteTitleVal: value }),
   // setNoteContentVal: (value) => set({ noteContentVal: value }),
-  seter: (value, type) => {
-    // if (type === "selectedTags") {
-    //   set((state) => ({ selectedTags: [...state.selectedTags!, value] }))
-    // }
-    // else if (type === "uploadImages") {
-    //   set((state) => ({ uploadImages: [...state.uploadImages!, ...value] }))}
-
-    set({ [type]: value })
-  },
-  resetValues: () =>
-    set({
-      noteTitleVal: "",
-      noteContentVal: "",
-      selectedTags: [],
-    }),
+  // if (type === "selectedTags") {
+  //   set((state) => ({ selectedTags: [...state.selectedTags!, value] }))
+  // }
+  // else if (type === "uploadImages") {
+  //   set((state) => ({ uploadImages: [...state.uploadImages!, ...value] }))}
 }))
-type EditDiaryData = {
-  mood: string
-  tag?: string[]
-  title?: string
-  content?: string
 
-  // setMood: (value: string) => void
-  // setTag: (value: string[]) => void
-  // setTitle: (value: string) => void
-  // setContent: (value: string) => void
+// type EditDiaryData = {
+//   mood: string
+//   tag?: string[]
+//   title?: string
+//   content?: string
 
-  seter: (value: string | string[], type: keyof EditDiaryData) => void
-}
-export const useEditDiaryValues = create<EditDiaryData>((set) => ({
-  mood: "",
-  tag: [],
-  title: "",
-  content: "",
-  seter: (value, type) => {
-    set({ [type]: value })
-  },
-  // setMood: (value) => set({ mood: value }),
-  // setTag: (value) => set({ tag: value }),
-  // setTitle: (value) => set({ title: value }),
-  // setContent: (value) => set({ content: value }),
-}))
+//   // setMood: (value: string) => void
+//   // setTag: (value: string[]) => void
+//   // setTitle: (value: string) => void
+//   // setContent: (value: string) => void
+
+//   seter: (value: string | string[], type: keyof EditDiaryData) => void
+// }
+// export const useEditDiaryValues = create<EditDiaryData>((set) => ({
+//   mood: "",
+//   tag: [],
+//   title: "",
+//   content: "",
+//   seter: (value, type) => {
+//     set({ [type]: value })
+//   },
+//   // setMood: (value) => set({ mood: value }),
+//   // setTag: (value) => set({ tag: value }),
+//   // setTitle: (value) => set({ title: value }),
+//   // setContent: (value) => set({ content: value }),
+// }))
