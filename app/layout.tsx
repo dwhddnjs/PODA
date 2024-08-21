b09d8febf491dfaa1cb7bdc6f05cd67bb7429ab5
qpsqps123 <55061153+qpsqps123@users.noreply.github.com>
2024-08-21 16:16:56 +0900

    🐛 Modify : metadata에 viewport가 적용되지 않는 문제 해결 #119

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next"
+import type { Metadata, Viewport } from "next"
 import { Inter as FontSans } from "next/font/google"
 import "./globals.css"
 import { cn } from "@/lib/utils"
@@ -66,10 +66,12 @@ const myFont = localFont({
   display: "swap",
 })
 
-export function generateViewport() {
-  return {
-    themeColor: "#ffffff",
-  }
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+  themeColor: "#272727",
 }
 
 export const metadata: Metadata = {
