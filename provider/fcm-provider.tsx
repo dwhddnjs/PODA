29baa132593378fb46ac5a762edfb5932092c3b3
dwhddnjs <per1215@naver.com>
2024-08-27 18:51:13 +0900

    🐛 배포 버그 수정

--- provider/fcm-provider.tsx
+++ provider/fcm-provider.tsx
@@ -30,7 +30,10 @@ export const FcmProvider = ({ children }: { children: React.ReactNode }) => {
 
   const app = initializeApp(firebaseConfig)
 
-  const messaging = getMessaging(app)
+  const messaging =
+    typeof window !== "undefined" &&
+    typeof window.navigator !== "undefined" &&
+    getMessaging(app)
 
   const fetchToken = async (userId: string) => {
     try {
@@ -54,10 +57,11 @@ export const FcmProvider = ({ children }: { children: React.ReactNode }) => {
 
   useEffect(() => {
     console.log("adsadsdas", data?.user)
-    if (data) {
-      fetchToken(data.user?._id!)
-    }
+
     if (typeof window !== "undefined" && "serviceWorker" in navigator) {
+      if (data) {
+        fetchToken(data.user?._id!)
+      }
       navigator.serviceWorker
         .register("/firebase-messaging-sw.js")
         .then((registration) => {
