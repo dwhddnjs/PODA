2083fd068bc1c6e790c61d482e58c00bf4a6e8b5
qpsqps123 <55061153+qpsqps123@users.noreply.github.com>
2024-08-27 16:45:28 +0900

    ✨ Feat : 사용자가 multi-select 페이지에 관심사 데이터를 선택하지 않으면 버튼이 비활성화 되도록 변경 #145

--- app/(onboarding)/get-extra-user-info/multi-select/[process]/page.tsx
+++ app/(onboarding)/get-extra-user-info/multi-select/[process]/page.tsx
@@ -6,6 +6,7 @@ import { Button } from "@/components/ui/button"
 import { useRouter } from "next/navigation"
 import { NavigationHeader } from "@/components/navigation-header"
 import { interests } from "@/components/interest-bottom-sheet"
+import { useUserData } from "@/hooks/store/use-user-data"
 
 export default function MultiSelectPage({
   params,
@@ -15,6 +16,8 @@ export default function MultiSelectPage({
   const pageNum = Number(params.process)
 
   const router = useRouter()
+
+  const { userData } = useUserData()
 
   return (
     <>
@@ -26,12 +29,21 @@ export default function MultiSelectPage({
           <Tag key={`t${index}`}>{interests[index]}</Tag>
         ))}
       </div>
-      <Button
-        type="button"
-        className="fixed bottom-6 w-5/6 max-w-96 left-1/2 -translate-x-1/2 bg-mainColor text-black font-bold "
-        onClick={() => router.push(`/login`)}>
-        완료
-      </Button>
+      {userData.interest?.length === 0 || userData.interest === undefined ? (
+        <Button
+          type="button"
+          className="fixed bottom-6 w-5/6 max-w-96 left-1/2 -translate-x-1/2 bg-mainColor text-black font-bold "
+          disabled>
+          완료
+        </Button>
+      ) : (
+        <Button
+          type="button"
+          className="fixed bottom-6 w-5/6 max-w-96 left-1/2 -translate-x-1/2 bg-mainColor text-black font-bold "
+          onClick={() => router.push(`/login`)}>
+          완료
+        </Button>
+      )}
     </>
   )
 }
